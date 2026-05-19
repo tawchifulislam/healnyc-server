4ded6b15b926bffd3e5337878e8fc192d939f4e9
Tawchif <tawchif04@gmail.com>
2026-05-19 18:03:45 +0600

    Feat: Implement CRUD for booking

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 8080;
 
 const uri = process.env.MONGODB_URI;
@@ -26,6 +27,7 @@ async function run() {
     // await client.db('admin').command({ ping: 1 });
     const db = client.db('healnycdb');
     const doctorsCollection = db.collection('doctors');
+    const bookingCollection = db.collection('booking');
 
     app.get('/doctors', async (req, res) => {
       const cursor = doctorsCollection.find();
@@ -44,6 +46,40 @@ async function run() {
       const doctorsId = req.params.doctorsId;
       const query = { _id: new ObjectId(doctorsId) };
       const result = await doctorsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/booking', async (req, res) => {
+      const bookingData = req.body;
+      const result = await bookingCollection.insertOne(bookingData);
+
+      res.json(result);
+    });
+
+    app.get('/booking/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const cursor = bookingCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.patch('/booking/:id', async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+
+      const result = await bookingCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+
+      res.send(result);
+    });
+
+    app.delete('/booking/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
